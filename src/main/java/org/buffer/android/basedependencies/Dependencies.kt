e4a12f21dc040bbca36d7b12bc1ee31e62219c0f
Victoria Gonda <v.m.gonda@gmail.com>
2019-06-27 15:56:15 -0500

    Use class again

--- src/main/java/org/buffer/android/basedependencies/Dependencies.kt
+++ src/main/java/org/buffer/android/basedependencies/Dependencies.kt
@@ -4,7 +4,7 @@ object Versions {
     const val retrofit = "2.5.0"
 }
 
-abstract class BaseDependencies {
+open class BaseDependencies {
 
-    open fun retrofit_converter() = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+    val retrofit_converter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
 }
